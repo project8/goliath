9e71379d32fdc43856a086ec9c75190fee3e580f
Jared Kofron <jared.kofron@gmail.com>
2011-09-19 16:39:32 -0700

    added circular buffer

---
+++ include/glth_cb.h
@@ -0,0 +1,6 @@
+/* glth_cb.c
+ * written by jared kofron <jared.kofron@gmail.com>
+ * circular buffer implementation for goliath.  used for the delay lines
+ * that the glth_fir finite impulse response filters use.
+ */
+
