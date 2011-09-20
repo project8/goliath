35b7d9f7d51ee870a26cfd3aecfcc9d0350e9ca8
Jared Kofron <jared.kofron@gmail.com>
2011-09-19 17:46:42 -0700

    added glth_cb_push declaration.  adds a value to a circular buffer and returns a result code indicating that args were OK

--- include/glth_cb.h
+++ include/glth_cb.h
@@ -4,3 +4,27 @@
  * that the glth_fir finite impulse response filters use.
  */
 
+// sys includes
+#include <stdlib.h>
+
+// goliath includes.
+#include "glth_sample.h"
+#include "glth_result.h"
+
+typedef struct {
+  unsigned int size;
+  unsigned int state;
+  glth_sample_t* buf;
+} glth_cb;
+
+/*
+ * initializes a circular buffer for use.  returns all is well if memory
+ * allocation was OK.
+ */
+glth_result glth_cb_init(glth_cb*, const unsigned int);
+
+/*
+ * pushes a value onto a circular buffer.  error generated if a null pointer
+ * is passed as the first argument.
+ */
+glth_result glth_cb_push(glth_cb*, const glth_sample_t);
