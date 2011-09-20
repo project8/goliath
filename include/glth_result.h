c01231804aa3b76b80621f4561454e1bd96393bb
Jared Kofron <jared.kofron@gmail.com>
2011-09-19 17:47:21 -0700

    added a result code header for goliath.

---
+++ include/glth_result.h
@@ -0,0 +1,24 @@
+/* glth_result.h
+ * written by jared kofron <jared.kofron@gmail.com>
+ * result codes for the goliath program.
+ */
+typedef short glth_result;
+
+/*
+ * values to be returned upon function success or
+ * failure.
+ */
+static const glth_result glth_success = 0x0;
+static const glth_result glth_failure = 0x1;
+
+/*
+ * this is returned if memory allocation fails for
+ * something.
+ */
+static const glth_result glth_e_nomem = 0x2;
+
+/* 
+ * return if a null pointer was passed to a function
+ * inappropriately.
+ */
+static const glth_result glth_nullptr = 0x3;
