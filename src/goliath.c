cb0407321afec0b89e604cf4821d5464e27a06a9
Jared Kofron <jared.kofron@gmail.com>
2011-09-20 13:20:52 -0700

    now parses options and prints to screen.  very fancy...

--- src/goliath.c
+++ src/goliath.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include "glth_opts.h"
 
-int main(const int argc, const char** argv) {
+int main(const int argc, char** argv) {
+  // first initialize the run information struct
+  glth_opts* run_info = (glth_opts*)calloc(1,sizeof(glth_opts));
+  if( args_to_opts(run_info, argc, argv) != glth_success ) {
+    printf("help!\n");
+  }
+  else print_glth_opts(run_info);
+
+  // done.
+  free(run_info);
+  return glth_success;
 }
