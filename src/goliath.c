97b7069ac8cb9aa32bdee05accecd656a3fa8b87
Jared Kofron <jared.kofron@gmail.com>
2011-09-20 14:23:09 -0700

    added a fir impulse test.

--- src/goliath.c
+++ src/goliath.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "glth_opts.h"
+#include "glth_fir.h"
 
 int main(const int argc, char** argv) {
   // first initialize the run information struct
@@ -8,6 +9,17 @@ int main(const int argc, char** argv) {
     printf("help!\n");
   }
   else print_glth_opts(run_info);
+
+  // just a test!
+  glth_fir* filter = (glth_fir*)calloc(1,sizeof(glth_fir));
+  glth_sample_t coefs[5] = {1.0,6.0,3.0,4.0,5.0};
+  glth_sample_t sig[11] = {0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0};
+  glth_sample_t output = (glth_sample_t)0;
+  glth_fir_init(filter,5,coefs);
+  for(unsigned int i = 0; i < 11; i++) {
+    glth_fir_update(filter,sig[i],&output);
+    printf("%f\n",output);
+  }
 
   // done.
   free(run_info);
