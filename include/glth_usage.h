78c56b99be6aede07b3280e59c02418e6a8b5154
Jared Kofron <jared.kofron@gmail.com>
2011-09-21 10:23:16 -0700

    added usage function

---
+++ include/glth_usage.h
@@ -0,0 +1,16 @@
+#ifndef __glth_usage_h
+#define __glth_usage_h
+
+
+/* 
+ * glth_usage.h
+ * written by jared kofron <jared.kofron@gmail.com>
+ * just prints a usage string to screen and exits.
+ */
+
+// sys includes
+#include <stdio.h>
+
+void glth_usage();
+
+#endif //__glth_usage_h
