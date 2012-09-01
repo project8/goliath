282cc308572b644c69873213035716e30f81bedc
Jared Kofron <jared.kofron@gmail.com>
2012-08-31 23:36:49 -0700

    added some more constants

--- include/glth_const.hpp
+++ include/glth_const.hpp
@@ -2,9 +2,20 @@
 #define __glth_const_hpp
 
 namespace glth_const {
-  enum exit {
-    success = 0
+  typedef enum {
+    exit_success = 0,
+    exit_env_failed = 1,
+    exit_io_failed = 2
+  } exit_status;
+
+  enum io_result {
+    io_success = 0
   };
+
+  typedef enum {
+    env_success = 0,
+    env_no_filename = 1
+  } env_result;
 
 } // glth_const namespace
 
