a9a4c193799ddf89f17a7f2a56044e94e52d3ea9
Jared Kofron <jared.kofron@gmail.com>
2012-08-31 23:38:28 -0700

    building up to functionality.

--- src/glth.cpp
+++ src/glth.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include "glth_io.hpp"
+#include "glth_env.hpp"
 #include "glth_signal.hpp"
 
-int main(const int argc, char** argv) {
+using glth::env;
+using glth::io;
+using namespace glth_const;
+
+int main(int argc, char** argv) {
   // By default we have succeeded.
-  glth_const::exit res = glth_const::success;
+  exit_status res = exit_success;
+
+  // OK now parse the arguments passed by the user and
+  // verify that they are sensible.
+  env const* env = env::env_from_args(argc, argv);
+
+  if( env::verify(*env) != env_success ) {
+    std::cout << "failed to create valid environment!" << std::endl;
+    res = exit_env_failed;
+  }
+
+  else {
+    io* io = io::open_file(env->get_in_filename());
+
+    if(io) {
+      std::cout << "hi" << std::endl;
+    }
+
+    // Otherwise opening the file barfed.  Exit ungracefully.
+    else {
+      std::cout << "couldn't open file: " 
+		<< env->get_in_filename() 
+		<< std::endl;
+      res = exit_io_failed;
+    }
+  }
 
   return res;
 }
