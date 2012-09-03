d4874f51bc5872ffd9339ba927cbe1b5a0fb84cb
Jared Kofron <jared.kofron@gmail.com>
2012-09-02 19:19:34 -0700

    update wvd signature for using tfr_data, change old wvd to xwvd

--- include/glth_xfrmr.hpp
+++ include/glth_xfrmr.hpp
@@ -2,6 +2,7 @@
 #define __glth_xfrmr_hpp
 
 #include "glth_signal.hpp"
+#include "glth_tfr_data.hpp"
 extern "C" {
   #include "fftw3.h"
 }
@@ -27,12 +28,12 @@ namespace glth {
 
     // Calculate the wigner-ville distribution for an input signal.  
     // Equivalent to xwvd(tgt, tgt, out).
-    void wvd(glth::signal tgt, std::complex<double>** out);
+    void wvd(glth::signal tgt, glth::tfr_data* out);
 
     // Calculate the cross wigner-ville distribution for two signals.
     void xwvd(glth::signal tgt1,
 	      glth::signal tgt2,
-	      std::complex<double>** out);
+	      glth::tfr_data* out);
   };
 
 }; // namespace glth
