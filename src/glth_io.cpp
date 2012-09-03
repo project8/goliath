3d11ecdcdceca5f45b64106068d1958a947c3209
Jared Kofron <jared.kofron@gmail.com>
2012-09-02 19:20:24 -0700

    lots of commits, mostly for populating signals from records

--- src/glth_io.cpp
+++ src/glth_io.cpp
@@ -13,6 +13,11 @@ glth::io* glth::io::open_file(std::string filename) {
     delete _io_ptr;
     _io_ptr = NULL;
   }
+
+  else if ( _io_ptr->_monarch_ptr->ReadHeader() == false ) {
+    delete _io_ptr;
+    _io_ptr = NULL;
+  }
   
   return _io_ptr;
 }
@@ -23,4 +28,71 @@ std::size_t glth::io::get_record_length() {
 
 std::size_t glth::io::get_record_length() const {
   return (this->_monarch_ptr->GetHeader()->GetRecordSize());
+}
+
+const Monarch const* glth::io::get_monarch_ptr() const {
+  return (this->_monarch_ptr);
+}
+
+const Monarch const* glth::io::get_monarch_ptr() {
+  return static_cast<const io*>(this)->get_monarch_ptr();
+}
+
+glth_const::io_result glth::io::populate(signal* ch1, signal* ch2) {
+  static const MonarchRecord* work_rec;
+
+  // The output arrays need to be of the correct shape.
+  if( (ch1->size()) != (this->get_record_length()) 
+      || (ch2->size()) != (this->get_record_length()) ) {
+    return glth_const::io_read_badarray;
+  }
+
+  // OK
+  else {
+    // Try to grab the next record.  It might be EOF, for example, but
+    // give it a shot.
+    if( this->_monarch_ptr->ReadRecord() == true ) {
+
+      // Grab the first channel of data.
+      work_rec = (this->_monarch_ptr->GetRecord());
+
+      // It had better be channel 1...
+      if(work_rec->fCId != 1) {
+	// fuck
+	return glth_const::io_channel_err;
+      }
+
+      // D2A the data from channel 1.
+      for(std::size_t idx = 0; idx < ch1->size(); idx++) {
+	double pt = 0;
+	ch1->at(idx) = glth_px1500::min_mvolts + 
+	  (glth_px1500::max_mvolts)*(work_rec->fDataPtr[idx])/(2<<glth_px1500::n_bits);
+      }
+
+      // OK, now we do it again.
+      if( this->_monarch_ptr->ReadRecord() == true ) {
+	// Grab the next channel of data.
+	work_rec = (this->_monarch_ptr->GetRecord());
+
+	// It had better be channel 2...
+	if(work_rec->fCId != 2) {
+	  // fuck
+	  return glth_const::io_channel_err;
+	}	
+
+	// And if it is, we D2A it again.
+	for(std::size_t idx = 0; idx < ch2->size(); idx++) {
+	  double pt = 0;
+	  ch2->at(idx) = glth_px1500::min_mvolts + 
+	    (glth_px1500::max_mvolts)*(work_rec->fDataPtr[idx])/(2<<glth_px1500::n_bits);
+	}
+      }
+
+      return glth_const::io_read_ok;
+    }
+
+    // At present, all we've got is EOF in terms of why reading could
+    // have failed.
+    else return glth_const::io_eof;
+  }
 }
