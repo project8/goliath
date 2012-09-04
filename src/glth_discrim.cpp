f40bfc8d5606878e6f8b73e5b6727caf974084ff
Jared Kofron <jared.kofron@gmail.com>
2012-09-04 13:21:29 -0700

    add implementation of any/2 function

--- src/glth_discrim.cpp
+++ src/glth_discrim.cpp
@@ -8,3 +8,11 @@ bool glth::discriminator::operator[](std::size_t index)
 {
   return (this->_bank.at(index));
 }
+
+bool glth::discriminator::any(std::size_t low, std::size_t high)
+{
+  for(std::size_t i = low; i < high; i++) {
+    if ((*this)[i]) return true;
+  }
+  return false;
+}
