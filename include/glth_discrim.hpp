6576e06fedfd6db4d760abff04ece62d27fe073b
Jared Kofron <jared.kofron@gmail.com>
2012-09-04 13:21:48 -0700

    declaration of any/2 function which checks a range of bits for high values

--- include/glth_discrim.hpp
+++ include/glth_discrim.hpp
@@ -5,6 +5,9 @@
  * The glth_discrim class abstracts away the concept of a
  * "discriminator", which is to say a bit or array of bits
  * that signify the presence or absence of some condition.
+ * discriminator supports checking a single bit by using the
+ * [] operator, or it can check a range of bits (OR'ed) by
+ * using the any(low,high) function.
  */
 
 #include <vector>
@@ -17,6 +20,7 @@ namespace glth {
   public:
     discriminator(std::size_t);
     bool operator[](std::size_t index);
+    bool any(std::size_t low, std::size_t high);
   }; // class discriminator
 
 }; // namespace glth
