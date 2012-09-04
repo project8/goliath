229df1646b592dc6cc54d209907cf8b82441109f
Jared Kofron <jared.kofron@gmail.com>
2012-09-04 16:54:03 -0700

    initialize bank to false

--- src/glth_discrim.cpp
+++ src/glth_discrim.cpp
@@ -2,17 +2,19 @@
 
 glth::discriminator::discriminator(std::size_t size) 
   : _bank(size)
-{ /* no-op */ }
+{ 
+  for(std::size_t i = 0; i < size; i++) _bank[i] = false;
+}
 
-bool glth::discriminator::operator[](std::size_t index)
+bool& glth::discriminator::operator[](std::size_t index)
 {
-  return (this->_bank.at(index));
+  return (this->_bank)[index];
 }
 
 bool glth::discriminator::any(std::size_t low, std::size_t high)
 {
   for(std::size_t i = low; i < high; i++) {
-    if ((*this)[i]) return true;
+    if ((this->_bank)[i] == true) return true;
   }
   return false;
 }
