ed4b3dd12b90a6fd06ac1dde58d1f582154243a0
Jared Kofron <jared.kofron@gmail.com>
2012-09-06 17:51:24 -0700

    switch from tfr_data being a standlone class to being a typedef for a map from a size_t to a signal

--- include/glth_tfr_data.hpp
+++ include/glth_tfr_data.hpp
@@ -2,25 +2,13 @@
 #define __glth_tfr_data_hpp
 
 #include <vector>
+#include <map>
 #include <complex>
+#include "glth_signal.hpp"
 
 namespace glth {
 
-  class tfr_data {
-  private:
-    std::size_t _ntime, _nfreq;
-    std::complex<double>** _data;
-  public:
-    tfr_data(std::size_t nt, std::size_t nf);
-
-    // size access
-    std::size_t get_ntime();
-    std::size_t get_nfreq();
-
-
-    // column access
-    std::complex<double>* operator[](std::size_t colidx);
-  };
+  typedef std::map<std::size_t,glth::signal> tfr_data;
   
 };
 
