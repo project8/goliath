6f04943a1a61e164cee73352e18caec2ddf3e8f3
Dan Furse <dlfurse@mit.edu>
2012-09-07 17:27:12 -0700

    xwvd seems to sort of work...

--- src/glth_xfrmr.cpp
+++ src/glth_xfrmr.cpp
@@ -10,14 +10,20 @@ glth::glth_xfrmr::glth_xfrmr( std::size_t size, std::size_t freq_bins, std::size
     _size( size ),
     _freq_bins( freq_bins ),
     _time_bins( time_bins ),
-    _aa_in( glth::signal( _size ) ),
-    _aa_out( glth::signal( _size ) ),
-    _aa_forward_plan( fftw_plan_dft_1d( _size, _aa_in, _aa_out, FFTW_FORWARD, FFTW_MEASURE ) ),
-    _aa_reverse_plan( fftw_plan_dft_1d( _size, _aa_out, _aa_in, FFTW_FORWARD, FFTW_MEASURE ) ),
-    _wvd_in( glth::signal( _freq_bins / 2 ) ),
-    _wvd_out( glth::signal( _freq_bins / 2 ) ),
-    _vwd_plan( fftw_plan_dft_1d( _freq_bins / 2, _wvd_in, _wvd_out, FFTW_FORWARD, FFTW_MEASURE ) )
+    _aa_sig_buf( glth::signal( _size ) ),
+    _aa_xfm_buf( glth::signal( _size ) ),
+    _aa_forward_plan( fftw_plan_dft_1d( _size, _aa_sig_buf, _aa_xfm_buf, FFTW_FORWARD, FFTW_MEASURE ) ),
+    _aa_reverse_plan( fftw_plan_dft_1d( _size, _aa_xfm_buf, _aa_sig_buf, FFTW_BACKWARD, FFTW_MEASURE ) ),
+    _wvd_in( glth::signal( _freq_bins ) ),
+    _wvd_out( glth::signal( _freq_bins ) ),
+    _wvd_plan( fftw_plan_dft_1d( _freq_bins, _wvd_in, _wvd_out, FFTW_FORWARD, FFTW_MEASURE ) )
 {
+}
+glth::glth_xfrmr::~glth_xfrmr()
+{
+    fftw_destroy_plan( _aa_forward_plan );
+    fftw_destroy_plan( _aa_reverse_plan );
+    fftw_destroy_plan( _wvd_plan );
 }
 
 void glth::glth_xfrmr::aa( const glth::signal& in, glth::signal& out )
@@ -26,7 +32,7 @@ void glth::glth_xfrmr::aa( const glth::signal& in, glth::signal& out )
     register size_t t_index;
 
     //copy in signal to buffer
-    _aa_in.copy_from( in );
+    _aa_sig_buf.copy_from( in );
 
     //compute forward transform
     fftw_execute( _aa_forward_plan );
@@ -34,82 +40,66 @@ void glth::glth_xfrmr::aa( const glth::signal& in, glth::signal& out )
     //filter
     for( t_index = 1; t_index < _size / 2; t_index++ )
     {
-        _aa_out[t_index][0] *= 2;
-        _aa_out[t_index][1] *= 2;
+        _aa_xfm_buf[t_index][0] *= 2;
+        _aa_xfm_buf[t_index][1] *= 2;
     }
     for( t_index = _size / 2 + 1; t_index < _size; t_index++ )
     {
-        _aa_out[t_index][0] = 0.;
-        _aa_in[t_index][1] = 0.;
+        _aa_xfm_buf[t_index][0] = 0.;
+        _aa_xfm_buf[t_index][1] = 0.;
     }
 
     //compute reverse transform
     fftw_execute( _aa_reverse_plan );
 
     //normalize
-    for( t_index = 1; t_index < _size / 2; t_index++ )
+    for( t_index = 0; t_index < _size; t_index++ )
     {
-        _aa_out[t_index][0] /= _size;
-        _aa_out[t_index][1] /= _size;
+        _aa_sig_buf[t_index][0] /= _size;
+        _aa_sig_buf[t_index][1] /= _size;
     }
 
     //copy buffer to out signal
-    _aa_out.copy_to( out );
+    _aa_sig_buf.copy_to( out );
 
     return;
 }
 
-void glth::glth_xfrmr::wvd( const glth::signal& tgt, glth::tfr_data& out )
+void glth::glth_xfrmr::wvd( const glth::signal& in, glth::signal& out, size_t offset )
 {
-    return this->xwvd( tgt, tgt, out );
+    return this->xwvd( in, in, out, offset );
 }
 
-void glth::glth_xfrmr::xwvd( const glth::signal& tgt1, const glth::signal& tgt2, glth::tfr_data& out )
+void glth::glth_xfrmr::xwvd( const glth::signal& in_1, const glth::signal& in_2, glth::signal& out, size_t offset )
 {
-    size_t time_stride = _size / _time_bins;
-    size_t time_index = 0;
-
-    cout << "  [xwvd] time stride <" << time_stride << ">" << endl;
-
     register double t1_real;
     register double t1_imag;
     register double t2_real;
     register double t2_imag;
+    register size_t tau_index_minus;
+    register size_t tau_index_plus;
 
-    for( size_t t = 0; t < _time_bins; t++ )
+    //compute the correlation between in_1 and in_2
+    tau_index_minus = 0;
+    tau_index_plus = _freq_bins - 1;
+    for( size_t j = 0; j < _freq_bins; j++ )
     {
-        _wvd_in.zero_all();
-        _wvd_out.zero_all();
-        for( size_t tau = 0; tau < _freq_bins / 2; tau++ )
-        {
-            t1_real = tgt1[time_index + tau][0];
-            t1_imag = tgt1[time_index + tau][1];
-            t2_real = tgt2[time_index + _freq_bins - 1 - tau][0];
-            t2_imag = tgt2[time_index + _freq_bins - 1 - tau][1];
-            if( t == 512 )
-            {
-                cout << " [xwvd] bin numbers are <" << time_index + tau << "> and <" << time_index + _freq_bins - 1 - tau << ">" << endl;
-            }
-            _wvd_in[tau][0] = t1_real * t2_real + t1_imag * t2_imag;
-            _wvd_in[tau][1] = t1_imag * t2_real - t1_real * t2_imag;
-        }
-        fftw_execute( _vwd_plan );
-        memcpy( out[t], _wvd_out.data(), _freq_bins * sizeof(cplx) );
-        if( t == 512 )
-        {
-            std::ofstream slice_real( "./slice_real.dat" );
-            std::ofstream slice_imag( "./slice_imag.dat" );
-            std::ofstream slice_norm( "./slice_norm.dat" );
-            for( size_t f = 0; f < _freq_bins; f++ )
-            {
-                slice_real << f << " " << out[512][f].real() << "\n";
-                slice_imag << f << " " << out[512][f].imag() << "\n";
-                slice_norm << f << " " << out[512][f].real() * out[512][f].real() + out[512][f].imag() * out[512][f].imag() << "\n";
-            }
-            slice_real.close();
-            slice_imag.close();
-            slice_norm.close();
-        }
-        time_index += time_stride;
+        t1_real = in_1[offset + tau_index_minus][0];
+        t1_imag = in_1[offset + tau_index_minus][1];
+        t2_real = in_2[offset + tau_index_plus][0];
+        t2_imag = in_2[offset + tau_index_plus][1];
+
+        _wvd_in[j][0] = t1_real * t2_real + t1_imag * t2_imag;
+        _wvd_in[j][1] = t1_imag * t2_real - t1_real * t2_imag;
+
+        tau_index_minus++;
+        tau_index_plus--;
     }
+
+    //execute the fft
+    fftw_execute( _wvd_plan );
+
+    //copy the output
+    _wvd_out.copy_to( out );
+    return;
 }
