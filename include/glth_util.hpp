1b01f8f25e29da0da3472e80ffdb307a67bcf880
Jared Kofron <jared.kofron@gmail.com>
2012-09-07 17:22:05 -0700

    added utility function to calculate the norm of our cplx type

---
+++ include/glth_util.hpp
@@ -0,0 +1,13 @@
+#ifndef __glth_util_hpp
+#define __glth_util_hpp
+
+#include "glth_signal.hpp"
+
+namespace glth {
+
+  double cplx_norm(const glth::cplx tgt);
+
+
+}; // namespace glth
+
+#endif // __glth_util_hpp
